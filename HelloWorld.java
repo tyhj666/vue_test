1babc046dbbd05501248019ea79b3350cb8cf6e1
tyhj666 <2390577531@qq.com>
2020-09-13 10:19:10 +0800

    vue在branch1分支上面新增了HelloWorld文件

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public void HelloWorld{
+	public static void main(Stirng[] args){
+		System.out.println("Hello World");
+		System.out.println("1");
+		System.out.println("Hello World");
+		System.out.println("2");
+	}
+}
